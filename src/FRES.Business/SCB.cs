a756a989ac3ea83bb2a30ba4d8b70fb6a8996c12
Rarve <rawitas@gmail.com>
2016-07-26 11:27:50 +0700

    Add files via upload

---
+++ src/FRES.Business/SCB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FRES.Business
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class SCB
+    {
+        public SCB()
+        {
+        }
+
+        public static void Extract()
+        {
+            Data.SCB.Extract();
+        }
+    }
+}
