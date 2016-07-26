f3eb5dd82910957734d62a8d0f67b1cf5784c592
Rarve <rawitas@gmail.com>
2016-07-26 11:28:24 +0700

    Add files via upload

---
+++ src/FRES.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FRES.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Business.SCB.Extract();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            System.Console.WriteLine("Finish");
+            System.Console.ReadKey();
+        }
+    }
+}
