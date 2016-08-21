8a208e7cd64e0e9f17a781a3f1c88e63edc6abee
Rawitas Krungkaew <rawitas.krungkaew@agoda.com>
2016-08-21 23:12:50 +0700

    push code

--- src/FRES.Source.Worker/Program.cs
+++ src/FRES.Source.Worker/Program.cs
@@ -13,28 +13,28 @@ namespace FRES.Source.E
             try
             {
                 Extractor[] extractors = new Extractor[] {
-                    //new Extract.SCB()
-                    //, new Extract.KTB()
-                    //, new Extract.TNB()
-                    //, new Extract.GSB()
-                    //new Extract.GHB() 
+                    // new Extract.SCB()
+                    //,new Extract.KTB()
+                    //,new Extract.TNB()
+                    //,new Extract.GSB()
+                    //,new Extract.GHB() 
                 };
 
                 foreach (var extractor in extractors)
                     extractor.Extract();
 
                 Transformer[] transformers = new Transformer[] {
-                    //new Transform.SCB()
-                    //new Transform.KTB()
-                    new Transform.TNB()
-                    //new Transform.GHB()
+                    // new Transform.GSB()
+                    //,new Transform.SCB()
+                    //,new Transform.KTB()
+                    //,new Transform.TNB()
                 };
 
                 foreach (var transformer in transformers)
                     transformer.Transform();
 
-                //var loader = new Loader();
-                //loader.GetStartedDemo().Wait();
+                var loader = new Loader();
+                loader.GetStartedDemo().Wait();
                 
             }
             catch (Exception ex)
