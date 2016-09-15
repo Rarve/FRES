98172cb5cc4bbb8d9d2b793bf201d4622e94701d
Rawitas Krungkaew <rawitas.krungkaew@agoda.com>
2016-09-15 12:00:55 +0700

    push code

---
+++ FRES.ETL/src/FRES.Structure/Map.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FRES.Structure
+{
+    public class Map
+    {
+        public Map()
+        {
+            Images = new List<string>();
+        }
+
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public List<string> Images { get; set; }
+        public string[] ParcelNumber { get; set; }
+        public string Desc { get; set; }
+        public string Number { get; set; }
+        public string Village { get; set; }
+        public string VillageNo { get; set; }
+        public string Alley { get; set; }
+        public string Lane { get; set; }
+        public string Road { get; set; }
+        public string SubDistrict { get; set; }
+        public string District { get; set; }
+        public string Province { get; set; }
+    }
+}
