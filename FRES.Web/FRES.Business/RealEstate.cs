e409a9bd3ff8b85810705e5e7583df93660ed5c8
Rawitas Krungkaew <rawitas.krungkaew@agoda.com>
2016-09-26 16:15:19 +0700

    push code

---
+++ FRES.Web/FRES.Business/RealEstate.cs
@@ -0,0 +1,16 @@
+﻿using FRES.Data.DocumentDB;
+using FRES.Structure;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FRES.Business
+{
+    public static class RealEstateBusiness
+    {
+        public static async Task<List<RealEstateObj>> GetAll()
+        {
+            var res = await DocumentUtils.GetAll();
+            return res;
+        }
+    }
+}
