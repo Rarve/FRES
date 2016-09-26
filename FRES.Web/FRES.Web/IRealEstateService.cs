57b4258c7458429ac93f827eba200079f42a9147
Rawitas Krungkaew <rawitas.krungkaew@agoda.com>
2016-09-26 16:17:21 +0700

    push code

---
+++ FRES.Web/FRES.Web/IRealEstateService.cs
@@ -0,0 +1,15 @@
+﻿using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRES.Web
+{
+    public interface IRealEstateService
+    {
+        [SwaggerOperation("GetAll")]
+        Task<string> GetAll();
+    }
+}
---
+++ FRES.Web/FRES.Web/RealEstateService.cs
@@ -0,0 +1,27 @@
+﻿using FRES.Common;
+using FRES.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FRES.Web
+{
+    public class RealEstateService : ApiController, IRealEstateService
+    {
+        public async Task<string> GetAll()
+        {
+            try
+            {
+                var res = await Business.RealEstateBusiness.GetAll();
+                return JsonHelper.Serialize(res);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
