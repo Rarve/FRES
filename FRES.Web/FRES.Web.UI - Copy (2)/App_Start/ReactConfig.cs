020d88f82b3ec336dd3e09fff772c9afe82ee69b
AGODA\rkrungkaew <rawitas.krungkaew@agoda.com>
2017-06-05 18:22:07 +0700

    push

---
+++ FRES.Web/FRES.Web.UI - Copy (2)/App_Start/ReactConfig.cs
@@ -0,0 +1,34 @@
+//using React;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(FRES.Web.UI.ReactConfig), "Configure")]
+
+namespace FRES.Web.UI
+{
+	public static class ReactConfig
+	{
+		public static void Configure()
+		{
+            // If you want to use server-side rendering of React components, 
+            // add all the necessary JavaScript files here. This includes 
+            // your components as well as all of their dependencies.
+            // See http://reactjs.net/ for more information. Example:
+            //ReactSiteConfiguration.Configuration
+            //	.AddScript("~/Scripts/First.jsx")
+            //	.AddScript("~/Scripts/Second.jsx");
+
+            // If you use an external build too (for example, Babel, Webpack,
+            // Browserify or Gulp), you can improve performance by disabling 
+            // ReactJS.NET's version of Babel and loading the pre-transpiled 
+            // scripts. Example:
+            //ReactSiteConfiguration.Configuration
+            //	.SetLoadBabel(false)
+            //	.AddScriptWithoutTransform("~/Scripts/bundle.server.js")
+
+            //ReactSiteConfiguration.Configuration
+            //    .AddScript("~/Scripts/First.jsx")
+            //    .AddScript("~/Scripts/Second.jsx")
+            //    .AddScript("~/node_modules//react-toolbox/lib/card/Card.js");
+
+        }
+    }
+}
