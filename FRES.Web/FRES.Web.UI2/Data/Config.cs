dd767cb64e04ce5b8c84eb09ff4d76c9c99fa76b
AGODA\rkrungkaew <rawitas.krungkaew@agoda.com>
2017-07-03 16:39:07 +0700

    push

--- FRES.Web/FRES.Web.UI2/Data/Config.cs
+++ FRES.Web/FRES.Web.UI2/Data/Config.cs
@@ -2,10 +2,10 @@
 {
     public class Config
     {
-        public static string EndPointUrl = "https://fresdocumentdb.documents.azure.com:443/";
-        public static string AuthorizationKey = "JyJIGLvVXWnyZlT10tbjE35UjuVS71zPdyNQvGBis2kJLj9ysTTkbM6Rn5a5OrlwZYP4BbKXfdhIXZAnerNtVg==";
-        public static string DatabaseId = "fresdb";
-        public static string CollectionId = "frescollection";
+        public static readonly string EndPointUrl = "https://fresdocumentdb.documents.azure.com:443/";
+        public static readonly string AuthorizationKey = "JyJIGLvVXWnyZlT10tbjE35UjuVS71zPdyNQvGBis2kJLj9ysTTkbM6Rn5a5OrlwZYP4BbKXfdhIXZAnerNtVg==";
+        public static readonly string DatabaseId = "fresdb";
+        public static readonly string CollectionId = "frescollection";
         
         public Config()
         {
---
+++ FRES.Web/NgApp1/README.md
@@ -0,0 +1,2 @@
+Something went wrong with the creation of an Angular CLI application.
+
